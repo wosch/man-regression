16078b6959c51c67156386a9b9f9f5f387c7854c
Wolfram Schneider <wosch@FreeBSD.org>
2024-01-07 18:04:48 +0000

    even more tests

--- basic.sh
+++ basic.sh
@@ -30,6 +30,7 @@ test $($man_command -w cat | wc -l) = 1
 test $($man_command -w cat man | wc -l) = 2
 
 test $($man_command cat | wc -l) -gt 70
+$man_command -S1 cat >/dev/null
 test $($man_command -S1 cat | wc -l) -gt 70
 
 if $man_command -S3 cat >/dev/null 2>&1; then
@@ -65,5 +66,12 @@ if $bug_page_spaces; then
   test $($man_command -M $man_dir -w "m a n" | wc -l) = 1
   test $($man_command -M $man_dir -w "c a t" "m a n" | wc -l) = 2
 fi
+
+$man_command -P cat cat >/dev/null
+$man_command 1 cat >/dev/null
+$man_command -a 1 cat man >/dev/null
+
+$man_command -a -P cat 1 man man man >/dev/null
+test $($man_command -a -P cat 1 man man man >/dev/null | wc -l) -gt 300
 
 #EOF
