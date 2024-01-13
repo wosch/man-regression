83d0394afb3249f028fd2753992bfdb7af7533a8
Wolfram Schneider <wosch@FreeBSD.org>
2024-01-13 15:26:42 +0000

    more tests

--- basic.sh
+++ basic.sh
@@ -70,12 +70,15 @@ else
 fi
 
 if $bug_page_spaces_new; then
+
 $man_command -P cat cat >/dev/null
 $man_command 1 cat >/dev/null
 $man_command -M /usr/share/man:/usr/share/man -a 1 cat man >/dev/null
 
 $man_command -M /usr/share/man:/usr/share/man -a -P cat 1 man man man >/dev/null
-test $($man_command -M /usr/share/man:/usr/share/man -a -P cat cat cat 2>/dev/null | wc -l) -ge 600
+
+# cat(1) manual page is 200 bytes long
+test $($man_command -M /usr/share/man:/usr/share/man -a -P cat cat cat cat 2>/dev/null | wc -l) -ge 600
 fi
 
 # temporary manpath
@@ -97,9 +100,24 @@ $man_command $man_dir/man1/"c a t.1" >/dev/null
 
 # run man(1) on page with spaces
 if $bug_page_spaces; then
-  test $($man_command -M $man_dir -w "c a t" | wc -l) = 1
-  test $($man_command -M $man_dir -w "m a n" | wc -l) = 1
-  test $($man_command -M $man_dir -w "c a t" "m a n" | wc -l) = 2
+test $($man_command -M $man_dir -w "c a t" | wc -l) = 1
+test $($man_command -M $man_dir -w "m a n" | wc -l) = 1
+test $($man_command -M $man_dir -w "c a t" "m a n" | wc -l) = 2
 fi
+
+# multiple copies of a manual pages (gzip'd or not)
+cp $($man_command -w cp) $man_dir/man1/cp.1.gz
+gunzip $man_dir/man1/cp.1.gz
+cp $($man_command -w cp) $man_dir/man1/cp.1.gz
+test $($man_command -M $man_dir -w cp | wc -l) = 1
+
+
+# multiple copies of a manual pages (gzip'd or not), with spaces
+cp $($man_command -w cp) $man_dir/man1/"c p.1.gz"
+gunzip $man_dir/man1/"c p.1.gz"
+cp $($man_command -w cp) $man_dir/man1/"c p.1.gz"
+#ls -l $man_dir/man1/
+#test $($man_command -M $man_dir -w "c p" | wc -l) = 1
+
 
 #EOF
